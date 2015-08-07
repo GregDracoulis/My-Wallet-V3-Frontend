4df728ac51d12c72461e27afa1bf7e07dc1e0c92
Karina <karina@blockchain.com>
2015-08-07 17:32:29 +0100

    added highchart-ng to karma.conf.js file to fix test

--- karma.conf.js
+++ karma.conf.js
@@ -29,8 +29,7 @@ module.exports = function(config){
       'tests/**/*.js',
       'app/templates/*.jade',
       'bower_components/angular-password-entropy/password-entropy.js',
-      'bower_components/chartjs/Chart.js',
-      'bower_components/angular-chart.js/angular-chart.js',
+      'bower_components/highcharts-ng/dist/highcharts-ng.js',
     ],
 
     autoWatch : true,
